78d0f07a7fc7a9f7a7c7fae9a5240cff6e5b5172
VWSCoronaDashboard15 <>
2021-05-28 11:37:59 +0200

    release 4.0.0

--- main/sql/views/infected_and_positive_tested/R__COA_VIEW__GGD_Tested_People_Average_NL.sql
+++ main/sql/views/infected_and_positive_tested/R__COA_VIEW__GGD_Tested_People_Average_NL.sql
@@ -3,12 +3,15 @@
 
 CREATE OR ALTER VIEW VWSDEST.V_GGD_TESTED_PEOPLE_AVERAGE_NL AS
 SELECT 
-       [NEW_DATE_OF_REPORT_UNIX] AS DATE_END_UNIX
-      ,CASE WHEN [DATE_RANGE_START_UNIX] IS NULL THEN (SELECT MIN(DATE_RANGE_START_UNIX) FROM [VWSDEST].[GGD_TESTED_PEOPLE_NL]) ELSE DATE_RANGE_START_UNIX END AS DATE_START_UNIX
-      ,[7D_AVERAGE_TESTS] AS TESTED_TOTAL
-      ,[7D_AVERAGE_POSITIVE_TESTS] AS INFECTED
-      ,ISNULL([7D_AVERAGE_PERCENTAGE_POSITIVE], 0) AS INFECTED_PERCENTAGE
-      ,dbo.CONVERT_DATETIME_TO_UNIX(DATE_LAST_INSERTED) AS DATE_OF_INSERTION_UNIX
-  FROM [VWSDEST].[GGD_TESTED_PEOPLE_NL]
+       [NEW_DATE_OF_REPORT_UNIX]                                                                                                                                  AS DATE_END_UNIX
+      ,CASE WHEN [DATE_RANGE_START_UNIX] IS NULL THEN (SELECT MIN(DATE_RANGE_START_UNIX) FROM [VWSDEST].[GGD_TESTED_PEOPLE_NL]) ELSE DATE_RANGE_START_UNIX END    AS DATE_START_UNIX
+      ,[POSITIVE_TESTS]                                                                                                                                           AS INFECTED
+      ,[7D_AVERAGE_POSITIVE_TESTS]                                                                                                                                AS INFECTED_MOVING_AVERAGE
+      ,[PERCENTAGE_POSITIVE]                                                                                                                                      AS INFECTED_PERCENTAGE
+      ,[7D_AVERAGE_PERCENTAGE_POSITIVE]                                                                                                                           AS INFECTED_PERCENTAGE_MOVING_AVERAGE
+      ,[TESTS]                                                                                                                                                    AS TESTED_TOTAL
+      ,[7D_AVERAGE_TESTS]                                                                                                                                         AS TESTED_TOTAL_MOVING_AVERAGE
+      ,dbo.CONVERT_DATETIME_TO_UNIX(DATE_LAST_INSERTED)                                                                                                           AS DATE_OF_INSERTION_UNIX
+FROM [VWSDEST].[GGD_TESTED_PEOPLE_NL]
 WHERE DATE_LAST_INSERTED = (SELECT max(DATE_LAST_INSERTED) 
                             FROM [VWSDEST].[GGD_TESTED_PEOPLE_NL])
--- main/sql/views/vaccine_stock/R__COA_VIEW__VACCINE_STOCK.sql
+++ main/sql/views/vaccine_stock/R__COA_VIEW__VACCINE_STOCK.sql
@@ -5,10 +5,11 @@ CREATE OR ALTER VIEW [VWSDEST].[V_RIVM_VACCINE_STOCK] AS
 WITH TOTAL_STOCK_CTE AS
 (
 	SELECT
-		 ISNULL([BioNTech/Pfizer],0) + ISNULL([Moderna],0) + ISNULL([AstraZeneca],0)			AS [total]
+		 ISNULL([BioNTech/Pfizer],0) + ISNULL([Moderna],0) + ISNULL([AstraZeneca],0) + ISNULL([Janssen],0)			AS [total]
 		,[BioNTech/Pfizer]										AS [bio_n_tech_pfizer]
 		,[Moderna]												AS [moderna]
 		,[AstraZeneca]											AS [astra_zeneca]
+		,[Janssen]											    AS [janssen]
 		,dbo.CONVERT_DATETIME_TO_UNIX([DATE_LAST_INSERTED])		AS [date_of_insertion_unix]
 		,dbo.CONVERT_DATETIME_TO_UNIX([DATE_OF_STATISTICS])		AS [date_unix]
 	FROM   
@@ -22,17 +23,18 @@ WITH TOTAL_STOCK_CTE AS
 		) vaccine_stock_pivot 
 	PIVOT  
 	(SUM(total_stock) FOR vaccine_name IN   
-		([BioNTech/Pfizer],[Moderna],[AstraZeneca])  
+		([BioNTech/Pfizer],[Moderna],[AstraZeneca],[Janssen])  
 	)AS unpvt
 )
 ,
 AVAILABLE_CTE AS
 (
 	SELECT
-		ISNULL([BioNTech/Pfizer],0) + ISNULL([Moderna],0) + ISNULL([AstraZeneca],0)			AS [total]
+		ISNULL([BioNTech/Pfizer],0) + ISNULL([Moderna],0) + ISNULL([AstraZeneca],0) + ISNULL([Janssen],0)			AS [total]
 		,[BioNTech/Pfizer]										AS [bio_n_tech_pfizer]
 		,[Moderna]												AS [moderna]
 		,[AstraZeneca]											AS [astra_zeneca]
+		,[Janssen]											    AS [janssen]
 		,dbo.CONVERT_DATETIME_TO_UNIX([DATE_LAST_INSERTED])		AS [date_of_insertion_unix]
 		,dbo.CONVERT_DATETIME_TO_UNIX([DATE_OF_STATISTICS])		AS [date_unix]
 	FROM   
@@ -46,7 +48,7 @@ AVAILABLE_CTE AS
 		) vaccine_stock_pivot 
 	PIVOT  
 	(SUM(free_stock) FOR vaccine_name IN   --Change total_stock to the new column containing available doses
-		([BioNTech/Pfizer],[Moderna],[AstraZeneca])  
+		([BioNTech/Pfizer],[Moderna],[AstraZeneca],[Janssen])  
 	)AS unpvt
 )
 SELECT 
@@ -54,14 +56,17 @@ SELECT
 		,T0.[bio_n_tech_pfizer]         AS bio_n_tech_pfizer_total
 		,T0.[moderna]                   AS moderna_total
 		,T0.[astra_zeneca]              AS astra_zeneca_total
+		,T0.[janssen]                   AS janssen_total
 		,T1.[total]                     AS total_available
 		,T1.[bio_n_tech_pfizer]         AS bio_n_tech_pfizer_available
 		,T1.[moderna]                   AS moderna_available
 		,T1.[astra_zeneca]              AS astra_zeneca_available
+		,T1.[janssen]                   AS janssen_available
         ,T0.[total] - T1.[total]                            AS total_not_available
 		,T0.[bio_n_tech_pfizer] - T1.[bio_n_tech_pfizer]    AS bio_n_tech_pfizer_not_available
 		,T0.[moderna] - T1.[moderna]                        AS moderna_not_available
 		,T0.[astra_zeneca] - T1.[astra_zeneca]              AS astra_zeneca_not_available
+		,T0.[janssen] - T1.[janssen]                        AS janssen_not_available
 		,T0.[date_of_insertion_unix]
 		,T0.[date_unix]
 FROM TOTAL_STOCK_CTE T0
