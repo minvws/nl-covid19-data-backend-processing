48edb3ae64e74581b0e668643f2031a4b9027bc2
mke <mke@netcompany.com>
2021-12-24 22:23:28 +0000

    clean up repo (for redesigning)

--- main/sql/tables/vwsstatic/V0_0_145__CREATE_TABLE__RWZI_GMCODE.sql
+++
@@ -1,9 +0,0 @@
--- Copyright (c) 2020 De Staat der Nederlanden, Ministerie van   Volksgezondheid, Welzijn en Sport.
--- Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2 - see https://github.com/minvws/nl-contact-tracing-app-coordinationfor more information.
-
-CREATE TABLE VWSSTATIC.RWZI_GMCODE(
-	[RWZI_CODE] VARCHAR(100) NOT NULL,
-	[GM_CODE] VARCHAR(100) NOT NULL,
-	DATE_LAST_INSERTED DATETIME DEFAULT GETDATE()
-)
-GO
