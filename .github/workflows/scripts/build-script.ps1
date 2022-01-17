4fc61bd15fdc71af3fc54e4ff6b8f3ea3f5d33f3
Miguel Keijzers <mke@netcompany.com>
2022-01-17 16:59:59 +0000

    change structure

--- .github/workflows/scripts/build-script.ps1
+++ .github/workflows/scripts/build-script.ps1
@@ -1,6 +1,6 @@
 Param (
     [String]$SourceDirectory = $env:PWD ?? $(Get-Location),
-    [String[]]$ModifiedFiles = $($(Get-ChildItem -Path "src/**/*.ipynb").FullName | ForEach-Object { $_ -replace "$($env:PWD ?? [regex]::escape($(Get-Location)))/", '' }),
+    [String[]]$ModifiedFiles = $($(Get-ChildItem -Path "src/**/*.ipynb" -Recurse).FullName | ForEach-Object { $_ -replace "$($env:PWD ?? [regex]::escape($(Get-Location)))/", '' }),
     [String]$DatatinoDevOpsPAT = $null,
     [String]$DatatinoDevOpsGitBranch = "main",
     [String]$DatatinoDevOpsGitUrl = "https://mke-netcompany@dev.azure.com/mke-netcompany/mke/_git/orchestrator",
@@ -26,7 +26,7 @@ foreach ($notebook in $notebooks) {
     }
 }
 
-$notebooks = $($deps | Select-Object -Unique)
+$notebooks = $($deps | Select-Object -Unique | ForEach-Object { return $(Get-ChildItem -Path $_).FullName } | Select-Object -Unique) 
 
 ### SETUP DATATINO MOCK CONTAINER(S).....
 Install-MssqlContainer `
@@ -88,7 +88,7 @@ if ($notebooks.Count -gt 0) {
                 Write-Host "Script build successfuly! `n" -ForegroundColor Green
             }
             catch {
-                Write-Error "$($_.exception.message)"
+                throw "$($_.exception.message)"
             }
         }
     }
