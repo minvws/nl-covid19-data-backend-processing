095500508a0ae6cdae3fd673f59e9d6b13b3bad3
mke <96122612+netcompany-mke@users.noreply.github.com>
2022-02-07 09:35:24 +0100

    Merge: release/sprint 40 to develop/ (#57) (#58)
    * Release sprint 40: "tussenrelease" - Annotations (#44)
    
    * add annotation (for PBI)
    
    * rename view
    
    * POWER-BI: (ECDC-Coverage) (#45)
    
    * add sql scripts
    
    * SemiColon -> Colon
    
    * POWER-BI: (ECDC-Coverage) #2 (#46)
    
    * change cron expression to 01:00 PM, only on Thursday
    
    * add column
    
    * RELEASE: Hotfix - recalculation municipalities (positive tested people) (#47)
    
    * adjust calculation and round to 1 decimal
    
    * fix dependency
    
    * fix decimal limit to 1
    
    * add logic to inactivate (or active) sewers (#48)
    
    * Revert "add logic to inactivate (or active) sewers (#48)" (#49)
    
    This reverts commit a92eb85e1077be985b1eeed482994ced0ffb51ff.
    
    * add logic to inactivate (or active) sewers (#50)
    
    * fix release - rwzi (#51)
    
    * update cohort mapping + activate booster_per_age_group workflow (#52)
    
    * fix release (#53)
    
    * Github Action: continue on error - when no proper error occurs
    
    * RELEASE: Hotfix/change endpoint spec for acc (#54)
    
    * add environment-deps variables
    
    * fix type (datatino config)
    
    * update filtering (#55)
    
    * deactivate protos (#56)
    
    * Add script for building locally using minikube
    
    Co-authored-by: Bart l' Amie <baam@netcompany.com>
    
    Co-authored-by: Bart l' Amie <baam@netcompany.com>

---
+++ .github/workflows/scripts/run-build-for-minikube.ps1
@@ -0,0 +1,89 @@
+﻿
+$tempDirectory = "C:\Projects\CoronaDashboard\Temp\Downloads\"
+$backendDirectory = "C:\Projects\CoronaDashboard\nl-covid19-data-backend-processing"
+$datatinoDirectory = "C:\Projects\CoronaDashboard\Datatino"
+
+$dockerServerName = "local-mssql"
+
+cd $backendDirectory
+
+Write-Host "Fetching docker environment variables from minikube..."  -ForegroundColor Yellow
+
+& minikube -p minikube docker-env --shell powershell | Invoke-Expression
+
+$sqlHost = ([System.Uri]$Env:DOCKER_HOST).Host
+
+Write-Host "Docker SQL host ip address: [$sqlHost]`n" -ForegroundColor Green
+Write-Host "Starting build script...`n"  -ForegroundColor Yellow
+
+.\.github\workflows\scripts\build-script.ps1 -Hostname $sqlHost
+
+######################################################################
+
+Write-Host "Finished build script. Determining SA password and SQL port number..."  -ForegroundColor Yellow
+
+$sqlPassword = $(docker exec $dockerServerName /bin/bash -c 'echo $MSSQL_SA_PASSWORD')
+$portNr = $(docker port $dockerServerName).Split(':')[-1]
+
+$datatinoLocalEnv = "$env:USERPROFILE\Datatino"
+
+######################################################################
+
+Write-Host "Updating connection string in local .env file: [$datatinoLocalEnv\.env]..."  -ForegroundColor Yellow
+$envSettings = Get-Content $datatinoLocalEnv\.env -Encoding UTF8 | ConvertFrom-Json
+
+$localSettings = $envSettings | ? {
+    $_.Level -eq 0
+} 
+
+$database =  $localSettings.availableDatabases | ? { $_.databaseSecretName -eq "DatatinoDatabase" }
+
+$database.databaseConnectionString = "DataSource=$sqlHost,$portNr;user id=SA;password=$sqlPassword;InitialCatalog=dashboard-db;IntegratedSecurity=True;MultipleActiveResultSets=True"
+
+$storageConnectionString = ($localSettings.availableStorages | ? { $_.storageSecretName -eq "DatatinoStorage" }).storageConnectionString
+
+ConvertTo-JSon @($envSettings) -Depth 100 | Out-File $datatinoLocalEnv\.env -fORCE
+
+Write-Host "Done saving file."  -ForegroundColor Green
+
+######################################################################
+
+Write-Host "Fetching .env file from azure blob storage..."  -ForegroundColor Yellow
+
+$storageContext = New-AzureStorageContext -connectionstring $storageConnectionString
+
+Get-AzureStorageBlobContent -Container "datatino" -Blob ".env" -Destination $tempDirectory -context $storageContext -Force
+
+Write-Host "`nUpdating connection string in temp file..." -ForegroundColor yellow 
+
+$envSettingsStorage = Get-Content $tempDirectory\.env -Encoding UTF8 | ConvertFrom-Json
+
+$localSettingsStorage = $envSettingsStorage | ? {
+    $_.Level -eq 0
+} 
+
+$databaseStorage =  $localSettingsStorage.availableDatabases | ? { $_.databaseSecretName -eq "DatatinoDatabase" }
+
+$databaseStorage.databaseConnectionString = "DataSource=$sqlHost,$portNr;user id=SA;password=$sqlPassword;InitialCatalog=dashboard-db;IntegratedSecurity=True;MultipleActiveResultSets=True"
+$localSettingsStorage.databaseConnectionString = "DataSource=$sqlHost,$portNr;user id=SA;password=$sqlPassword;InitialCatalog=dashboard-db;IntegratedSecurity=True;MultipleActiveResultSets=True"
+
+
+ConvertTo-JSon @($envSettingsStorage) -Depth 100 | Out-File $tempDirectory\.env -Force
+
+Write-Host "Uploading temp file to blob...`n" -ForegroundColor Yellow
+
+Set-AzureStorageBlobContent -Container "datatino" -Blob ".env" -File "$tempDirectory\.env" -context $storageContext -Force
+
+Write-Host "`nDone uploading.`n" -ForegroundColor Green
+
+######################################################################
+
+Write-Host "Updating EF core config in the Datatino.Model project: [$datatinoDirectory\Datatino.Model\.database]..." -ForegroundColor Yellow
+
+$datatinoSettings = Get-Content $datatinoDirectory\Datatino.Model\.database -Encoding UTF8 | ConvertFrom-Json
+
+$datatinoSettings.DatabaseConnectionString = "DataSource=$sqlHost,$portNr;user id=SA;password=$sqlPassword;InitialCatalog=dashboard-db;IntegratedSecurity=True;MultipleActiveResultSets=True"
+
+ConvertTo-JSon $datatinoSettings -Depth 100 | Out-File $datatinoDirectory\Datatino.Model\.database -Force
+
+Write-Host "`nAll done." -ForegroundColor Green
