a796f6c44ab1b5467222b8cef59cd02bd8941492
netcompany-mke <mke@netcompany.com>
2022-07-15 21:03:53 +0000

    update build script and sql scripts (try-out)

--- .devops/scripts/build-script.ps1
+++ .devops/scripts/build-script.ps1
@@ -1,6 +1,6 @@
 Param (
     [String]$SourceDirectory = $env:PWD ?? $(Get-Location),
-    [String[]]$ModifiedFiles = $($(Get-ChildItem -Path "src/**/*.ipynb" -Recurse).FullName | ForEach-Object { $_ -replace "$($env:PWD ?? [regex]::escape($(Get-Location)))/", '' }),
+    [String[]]$ModifiedFiles = $null,
     [String]$DatatinoDevOpsPAT = $null,
     [String]$DatatinoDevOpsGitBranch = "master",
     [String]$DatatinoDevOpsGitRefUrl = "https://VWSCoronaDashboard@dev.azure.com/VWSCoronaDashboard/Corona Dashboard/_git/nl-cdb-be-apis",
@@ -17,7 +17,13 @@ $serverName = "local-mssql"
 $serverPort = $Port
 
 ### GET MODIFIED NOTEBOOK(S).....
-$notebooks = $ModifiedFiles | Where-Object { ($_.Endswith(".ipynb")) -and (Test-Path -LiteralPath $_) -and (-not [System.IO.Path]::GetFileName($_).StartsWith("__")) }
+if (($null -eq $ModifiedFiles) -or ($($ModifiedFiles | Where-Object { $_.Trim() -ne [System.String]::Empty}).Count -eq 0))  {
+    $files = $(Get-ChildItem -Path "src/**/*.ipynb" -Recurse).FullName | ForEach-Object { $_ -replace "$($env:PWD ?? [regex]::escape($(Get-Location)))/", '' }
+} else {
+    $files = $ModifiedFiles
+}
+
+$notebooks = $files | Where-Object { ($_.Endswith(".ipynb")) -and (Test-Path -LiteralPath $_) -and (-not [System.IO.Path]::GetFileName($_).StartsWith("__")) }
 
 $deps = @()
 foreach ($notebook in $notebooks) {
