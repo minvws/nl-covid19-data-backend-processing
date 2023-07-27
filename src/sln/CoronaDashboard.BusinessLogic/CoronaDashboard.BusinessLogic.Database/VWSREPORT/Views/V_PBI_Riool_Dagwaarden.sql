4d6515e94ec38dd7cc0f1cab4b7f67f363b4515e
Bart l' Amie <baam@netcompany.com>
2023-07-27 14:07:42 +0200

    Update database based on changes to production for sewers

--- src/sln/CoronaDashboard.BusinessLogic/CoronaDashboard.BusinessLogic.Database/VWSREPORT/Views/V_PBI_Riool_Dagwaarden.sql
+++ src/sln/CoronaDashboard.BusinessLogic/CoronaDashboard.BusinessLogic.Database/VWSREPORT/Views/V_PBI_Riool_Dagwaarden.sql
@@ -1,16 +1,16 @@
-﻿CREATE   VIEW [VWSREPORT].[V_PBI_Riool_Dagwaarden]
+﻿-- Copyright (c) 2020 De Staat der Nederlanden, Ministerie van   Volksgezondheid, Welzijn en Sport. 
+ -- Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2 - see https://github.com/minvws/nl-contact-tracing-app-coordination for more information.
+ 
+ CREATE   VIEW [VWSREPORT].[V_PBI_Riool_Dagwaarden]
  AS
- -- Riool gegevens      
+ -- Riool gegevens
      SELECT
-         CAST([DATE_MEASUREMENT] as date)                                        AS [Datum]
-         ,CAST(dbo.CONVERT_UNIX_TO_DATETIME([WEEK_UNIX]) as date)	            AS [Week start]
-         ,CAST(dbo.WEEK_END(dbo.CONVERT_UNIX_TO_DATETIME([WEEK_UNIX])) as date)  AS [Week einde]
-         ,CAST([DATE_LAST_INSERTED] as date)                                     AS [Update datum]
-         ,[AVERAGE_RNA_FLOW_PER_100000]							                AS [RNA flow per 100K]
-         ,[REGION_COVERED_INHABITANTS]								            AS [Inwoners regio covered]
-         ,[RNA_FLOW_PER_100000_PER_RWZI_ORIGINAL]                                AS [RNA flow per 100K dagwaarde]
-   FROM [VWSDEST].[SEWER_PROLONGATED]
-   WHERE
- 	[DATE_LAST_INSERTED] = (SELECT MAX([DATE_LAST_INSERTED]) FROM [VWSDEST].[SEWER_PROLONGATED])
- 	and
- 	REGIO_CODE = 'NL'
+         CAST([DATE_MEASUREMENT] AS DATE)                                       AS [Datum]
+         ,CAST(dbo.WEEK_START([DATE_MEASUREMENT]) AS DATE)                      AS [Week start]
+         ,CAST(dbo.WEEK_END([DATE_MEASUREMENT]) AS DATE)                        AS [Week einde]
+         ,CAST([DATE_LAST_INSERTED] AS DATE)                                    AS [Update datum]
+         ,CAST(([RNA_FLOW_PER_100000] / 1.0E+11) AS [DECIMAL](16, 3))           AS [RNA flow per 100K]
+         ,NULL                                                                  AS [Inwoners regio covered]
+         ,NULL                                                                  AS [RNA flow per 100K dagwaarde]
+   FROM [VWSDEST].[RIVM_SEWER_MEASUREMENTS_NL_2023]
+   WHERE [DATE_LAST_INSERTED] = (SELECT MAX([DATE_LAST_INSERTED]) FROM [VWSDEST].[RIVM_SEWER_MEASUREMENTS_NL_2023])
